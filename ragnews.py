3ce5e78e53431cc3dfe6a1ccd0c80a3d23510bb4
ains-arch <ainsleearchibald@gmail.com>
2024-09-15 23:42:28 -0700

    database formatting

--- ragnews.py
+++ ragnews.py
@@ -394,7 +394,7 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--loglevel', default='warning')
-    parser.add_argument('--db', default='ragnews.db')
+    parser.add_argument('--db', default='databases/ragnews.db')
     parser.add_argument('--recursive_depth', default=0, type=int)
     parser.add_argument('--add_url', help='''If this parameter is added, then the program will not
     provide an interactive QA session with the database.  Instead, the provided url will be
