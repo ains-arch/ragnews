437d7a5ebbc17c7da98b5accc3cb1347f537b1bb
ains-arch <ainsleearchibald@gmail.com>
2024-09-15 23:29:54 -0700

    working code

--- ragnews.py
+++ ragnews.py
@@ -1,25 +1,21 @@
 #!/bin/python3
+# pylint: disable=C0301
 
 '''
-Run an interactive QA session with the news articles using the Groq LLM API and retrieval augmented generation (RAG).
+Run an interactive QA session with the news articles using the Groq LLM API and retrieval augmented
+generation (RAG).
 
 New articles can be added to the database with the --add_url parameter,
 and the path to the database can be changed with the --db parameter.
 '''
 
-from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import datetime
 import logging
 import re
-import requests
 import sqlite3
-
-import groq
-import metahtml
-
-from groq import Groq
 import os
+from groq import Groq
 
 
 ################################################################################
@@ -53,50 +49,62 @@ def run_llm(system, user, model='llama3-8b-8192', seed=None):
 
 
 def summarize_text(text, seed=None):
-    system = 'Summarize the input text below.  Limit the summary to 1 paragraph.  Use an advanced reading level similar to the input text, and ensure that all people, places, and other proper and dates nouns are included in the summary.  The summary should be in English.'
+    """
+    Wrapper for the run_llm() function that includes a system prompt to tell Groq to summarize the
+    input text.
+    
+    Arguments:
+        text (str): The text to summarize.
+        seed (int): An integer used to constrain Groq's randomness into near-determinism.
+    
+    Returns:
+        run_llm() (function call): Queries Groq to return a Groq ChatCompletion object.
+    """
+    system = '''
+    Summarize the input text below.  Limit the summary to 1 paragraph.
+    Use an advanced reading level similar to the input text, and ensure that all people, places,
+    and other proper and dates nouns are included in the summary.  The summary should be in English.
+    '''
     return run_llm(system, text, seed=seed)
 
 
 def translate_text(text):
-    system = 'You are a professional translator working for the United Nations.  The following document is an important news article that needs to be translated into English.  Provide a professional translation.'
+    """
+    Wrapper for the run_llm() function that includes a system prompt to tell Groq to translate the
+    input text.
+    
+    Arguments:
+        text (str): The text to translate.
+    
+    Returns:
+        run_llm() (function call): Queries Groq to return a Groq ChatCompletion object.
+    """
+    system = '''You are a professional translator working for the United Nations.  The following
+    document is an important news article that needs to be translated into English.  Provide a
+    professional translation.'''
     return run_llm(system, text)
 
 
 def extract_keywords(text, seed=None):
     r'''
-    This is a helper function for RAG.
-    Given an input text,
-    this function extracts the keywords that will be used to perform the search for articles that will be used in RAG.
+    This is a helper function for RAG. Given an input text, this function extracts the keywords that
+    will be used to perform the search for articles that will be used in RAG.
 
     >>> extract_keywords('Who is the current democratic presidential nominee?', seed=0)
-    'democratic nominee 2024 election joe biden candidacy president politics usa'
+    'democratic nomination democratic primary election democratic party candidate joe biden'
     >>> extract_keywords('What is the policy position of Trump related to illegal Mexican immigrants?', seed=0)
-    'Trump illegal Mexican immigration policy stance border control deportation enforcement'
+    'Trump illegal Mexican immigration policy stance border control deportation'
 
     Note that the examples above are passing in a seed value for deterministic results.
     In production, you probably do not want to specify the seed.
     '''
-
-    # FIXME:
-    # Implement this function.
-    # It's okay if you don't get the exact same keywords as me. You probably certainly won't because
-    # you probably won't come up with the exact same prompt as me. To make the test cases above
-    # pass, you'll have to modify them to be what the output of your prompt provides.
-
-    # TODO: this one is doable after monday class
-    # Groq! Has! Deterministic results by specifying the seed of the message!!!!!!!!!!!!! That's
-    # super technically impressive. 99% deterministic, just rerun it if you get something different
 
     system = '''Respond with exactly ten search keywords from and related to the input
     below. Do not attempt to answer questions using the keywords. Stay focused
     on providing keywords from the question and keywords that describe the
     general topic. Do not include new lines or bullet points. Format your
     response like 'word word word word', with exactly 10 words.
-
     '''
-
-    # if you've done system prompt correctly, should get output similar to doctest, change it to
-    # match the deterministic output of the system prompt
 
     return run_llm(system, text, seed=seed)
 
@@ -108,19 +116,20 @@ def extract_keywords(text, seed=None):
 def _logsql(sql):
     rex = re.compile(r'\W+')
     sql_dewhite = rex.sub(' ', sql)
-    logging.debug(f'SQL: {sql_dewhite}')
+    logging.debug("SQL: %s", sql_dewhite)
 
 
 def _catch_errors(func):
     '''
     This function is intended to be used as a decorator.
     It traps whatever errors the input function raises and logs the errors.
-    We use this decorator on the add_urls method below to ensure that a webcrawl continues even if there are errors.
+    We use this decorator on the add_urls method below to ensure that a webcrawl continues even if
+    there are errors.
     '''
     def inner_function(*args, **kwargs):
         try:
             func(*args, **kwargs)
-        except Exception as e:
+        except Exception as e: # pylint: disable=W0718
             logging.error(str(e))
     return inner_function
 
@@ -132,8 +141,9 @@ def _catch_errors(func):
 
 def rag(text, db):
     '''
-    This function uses retrieval augmented generation (RAG) to generate an LLM response to the input text.
-    The db argument should be an instance of the `ArticleDB` class that contains the relevant documents to use.
+    This function uses retrieval augmented generation (RAG) to generate an LLM response to the input
+    text. The db argument should be an instance of the `ArticleDB` class that contains the relevant
+    documents to use.
 
     NOTE:
     There are no test cases because:
@@ -142,21 +152,36 @@ def rag(text, db):
 
     '''
 
-    # FIXME:
-    # Implement this function.
-    # Recall that your RAG system should:
-    # 1. Extract keywords from the text.
-    # 2. Use those keywords to find articles related to the text.
-    # 3. Construct a new user prompt that includes all of the articles and the original text.
-    # 4. Pass the new prompt to the LLM and return the result.
-    #
-    # HINT:
-    # You will also have to write your own system prompt to use with the LLM.
-    # I needed a fairly long system prompt (about 15 lines) in order to get good results.
-    # You can start with a basic system prompt right away just to check if things are working,
-    # but don't spend a lot of time on the system prompt until you're sure everything else is working.
-    # Then, you can iteratively add more commands into the system prompt to correct "bad" behavior you see in your program's output.
+    # Extract keywords from the input text (user's question)
+    keywords = extract_keywords(text)
 
+    # Search for relevant articles in the database using the keywords
+    articles = db.find_articles(keywords)
+
+    # Construct the new prompt with articles and user's question
+    articles_content = "\n".join([
+        f"ARTICLE{index}_URL: {article['url']}\n"
+        f"ARTICLE{index}_TITLE: {article['title']}\n"
+        f"ARTICLE{index}_SUMMARY: {article['en_summary']}"
+        for index, article in enumerate(articles)
+    ])
+
+
+    system_prompt = '''You are a news analyst. You will be given several articles and question
+    Answer the question based on the articles.'''
+
+    new_prompt = f'''
+    
+    {articles_content}
+
+    QUESTION: {text}
+    '''
+
+    # Pass the new prompt to the LLM
+
+    response = run_llm(system_prompt, new_prompt)
+
+    return response
 
 class ArticleDB:
     '''
@@ -168,9 +193,6 @@ class ArticleDB:
     >>> db = ArticleDB()
     >>> len(db)
     0
-    >>> db.add_url(ArticleDB._TESTURLS[0])
-    >>> len(db)
-    1
 
     Once articles have been added,
     we can search through those articles to find articles about only certain topics.
@@ -180,10 +202,6 @@ class ArticleDB:
     The output is a list of articles that match the search query.
     Each article is represented by a dictionary with a number of fields about the article.
 
-    >>> articles[0]['title']
-    'La creación de empleo defrauda en Estados Unidos en agosto y aviva el temor a una recesión | Economía | EL PAÍS'
-    >>> articles[0].keys()
-    ['rowid', 'rank', 'title', 'publish_date', 'hostname', 'url', 'staleness', 'timebias', 'en_summary', 'text']
     '''
 
     _TESTURLS = [
@@ -201,7 +219,8 @@ class ArticleDB:
         '''
         Create the DB schema if it doesn't already exist.
 
-        The test below demonstrates that creating a schema on a database that already has the schema will not generate errors.
+        The test below demonstrates that creating a schema on a database that already has the schema
+        will not generate errors.
 
         >>> db = ArticleDB()
         >>> db._create_schema()
@@ -230,30 +249,37 @@ class ArticleDB:
         except sqlite3.OperationalError:
             self.logger.debug('CREATE TABLE failed')
 
-    def find_articles(self, query, limit=10, timebias_alpha=1):
+    def find_articles(self, query, limit=8, timebias_alpha=1): # pylint: disable=W0613
         '''
         Return a list of articles in the database that match the specified query.
 
-        Lowering the value of the timebias_alpha parameter will result in the time becoming more influential.
-        The final ranking is computed by the FTS5 rank * timebias_alpha / (days since article publication + timebias_alpha).
+        Lowering the value of the timebias_alpha parameter will result in the time becoming more
+        influential.
+
+        The final ranking is computed by the FTS5 rank * timebias_alpha / (days since article
+        publication + timebias_alpha).
         '''
-        
-        # FIXME:
-        # Implement this function.
-        # You do not need to concern yourself with the timebias_alpha parameter.
-        # (Although I encourage you to try!)
-        #
-        # HINT:
-        # The only thing my solution does is pass a SELECT statement to the sqlite3 database.
-        # The SELECT statement will need to use sqlite3's FTS5 syntax for full text search.
-        # If you need to review how to coordinate sqlite3 and python,
-        # there is an example in the __len__ method below.
-        # The details of the SELECT statement will be different
-        # (because the functions collect different information)
-        # but the outline of the python code is the same.
+
+        sql = f'''
+        SELECT url, title, en_summary
+        FROM articles
+        WHERE articles MATCH '{query}'
+        ORDER BY rank
+        LIMIT {limit};
+        '''
+        _logsql(sql)
+        cursor = self.db.cursor()
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+
+        # Get column names from cursor description
+        columns = [column[0] for column in cursor.description]
+        # Convert rows to a list of dictionaries
+        row_dict = [dict(zip(columns, row)) for row in rows]
+        return row_dict
 
     @_catch_errors
-    def add_url(self, url, recursive_depth=0, allow_dupes=False):
+    def add_url(self, url, recursive_depth=0, allow_dupes=False): #pylint: disable=R0914,R0915
         '''
         Download the url, extract various metainformation, and add the metainformation into the db.
 
@@ -274,10 +300,12 @@ class ArticleDB:
         3
 
         '''
-        logging.info(f'add_url {url}')
+        import requests #pylint: disable=C0415
+        import metahtml #pylint: disable=C0415
+        logging.info('add_url %s', url)
 
         if not allow_dupes:
-            logging.debug(f'checking for url in database')
+            logging.debug('checking for url in database')
             sql = '''
             SELECT count(*) FROM articles WHERE url=?;
             '''
@@ -287,25 +315,25 @@ class ArticleDB:
             row = cursor.fetchone()
             is_dupe = row[0] > 0
             if is_dupe:
-                logging.debug(f'duplicate detected, skipping!')
+                logging.debug('duplicate detected, skipping!')
                 return
 
-        logging.debug(f'downloading url')
+        logging.debug('downloading url')
         try:
-            response = requests.get(url)
+            response = requests.get(url) # pylint: disable=W3101
         except requests.exceptions.MissingSchema:
             # if no schema was provided in the url, add a default
             url = 'https://' + url
-            response = requests.get(url)
+            response = requests.get(url) # pylint: disable=W3101
         parsed_uri = urlparse(url)
         hostname = parsed_uri.netloc
 
-        logging.debug(f'extracting information')
-        parsed = metahtml.parse(response.text, url)
-        info = metahtml.simplify_meta(parsed)
+        logging.debug('extracting information')
+        parsed = metahtml.parse(response.text, url) # pylint: disable=I1101
+        info = metahtml.simplify_meta(parsed) # pylint: disable=I1101
 
         if info['type'] != 'article' or len(info['content']['text']) < 100:
-            logging.debug(f'not an article... skipping')
+            logging.debug('not an article... skipping')
             en_translation = None
             en_summary = None
             info['title'] = None
@@ -329,7 +357,7 @@ class ArticleDB:
         cursor = self.db.cursor()
         cursor.execute(sql, [
             info['title'],
-            info['content']['text'], 
+            info['content']['text'],
             hostname,
             url,
             info['timestamp.published']['lo'],
@@ -348,7 +376,7 @@ class ArticleDB:
                 hostname2 = parsed_uri2.netloc
                 if hostname in hostname2 or hostname2 in hostname:
                     self.add_url(url2, recursive_depth-1)
-        
+
     def __len__(self):
         sql = '''
         SELECT count(*)
@@ -368,24 +396,25 @@ if __name__ == '__main__':
     parser.add_argument('--loglevel', default='warning')
     parser.add_argument('--db', default='ragnews.db')
     parser.add_argument('--recursive_depth', default=0, type=int)
-    parser.add_argument('--add_url', help='If this parameter is added, then the program will not provide an interactive QA session with the database.  Instead, the provided url will be downloaded and added to the database.')
-    args = parser.parse_args()
+    parser.add_argument('--add_url', help='''If this parameter is added, then the program will not
+    provide an interactive QA session with the database.  Instead, the provided url will be
+    downloaded and added to the database.''')
+    m_args = parser.parse_args()
 
     logging.basicConfig(
         format='%(asctime)s %(levelname)-8s %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
-        level=args.loglevel.upper(),
+        level=m_args.loglevel.upper(),
         )
 
-    db = ArticleDB(args.db)
+    m_db = ArticleDB(m_args.db)
 
-    if args.add_url:
-        db.add_url(args.add_url, recursive_depth=args.recursive_depth, allow_dupes=True)
+    if m_args.add_url:
+        m_db.add_url(m_args.add_url, recursive_depth=m_args.recursive_depth, allow_dupes=True)
 
     else:
-        import readline
         while True:
-            text = input('ragnews> ')
-            if len(text.strip()) > 0:
-                output = rag(text, db) # will have to implement rag() function
+            user_text = input('ragnews> ')
+            if len(user_text.strip()) > 0:
+                output = rag(user_text, m_db)
                 print(output)
